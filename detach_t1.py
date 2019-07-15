35690cdae9b10c5512e5158aad4f1f8f5e7fc38a
root <root@ansible.core.hypervizor.com>
2019-07-14 21:14:53 -0700

    initial commit

---
+++ detach_t1.py
@@ -0,0 +1,28 @@
+
+####################################
+#
+###################################
+
+import requests
+import json
+from requests.auth import HTTPBasicAuth
+
+username = 'admin'
+password = 'VMware1!VMware1!'
+t1_gw = 'T1-GW'
+t0_gw = 'T0-GW-01'
+nsxtmgr = "nsxtmgr01"
+auth = HTTPBasicAuth(username, password)
+headers = { 'content-type': 'application/json' }
+
+def disconnect_t0():
+    url = 'https://' + nsxtmgr + '/policy/api/v1/infra/tier-1s/' + t1_gw    
+    payload = { "tier0_path": "None" }
+    response = requests.patch(url, data=json.dumps(payload), headers=headers,  verify=False, auth=auth)
+    return response
+
+if __name__ == '__main__':
+
+    rc = disconnect_t0()
+    print rc.status_code
+
