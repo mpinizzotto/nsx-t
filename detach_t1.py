dc379fd83693571ea1a56c0b5c279eee114b3464
root <root@ansible.core.hypervizor.com>
2019-07-16 19:43:22 -0700

    added main()

--- detach_t1.py
+++ detach_t1.py
@@ -6,27 +6,31 @@ _author_ = 'mpinizzotto'
 
 #Detach T1 gateway from T0 gateway
 
-
 import requests
+requests.packages.urllib3.disable_warnings() 
 import json
 from requests.auth import HTTPBasicAuth
 
+
+headers = { 'content-type': 'application/json' }
 username = 'admin'
 password = 'VMware1!VMware1!'
 t1_gw = 'T1-GW'
 t0_gw = 'T0-GW-01'
 nsxtmgr = "nsxtmgr01"
 auth = HTTPBasicAuth(username, password)
-headers = { 'content-type': 'application/json' }
 
-def disconnect_t0():
+
+def disconnect_t0(t1_gw):
     url = 'https://' + nsxtmgr + '/policy/api/v1/infra/tier-1s/' + t1_gw    
     payload = { "tier0_path": "None" }
     response = requests.patch(url, data=json.dumps(payload), headers=headers,  verify=False, auth=auth)
     return response
 
+def main():
+    response = disconnect_t0(t1_gw)
+    #print response
+    
+
 if __name__ == '__main__':
-
-    rc = disconnect_t0()
-    print rc.status_code
-
+    main()
