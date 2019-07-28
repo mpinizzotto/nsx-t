3899f44b3e40d83bc047dd3dd9eb12062cc4e044
root <root@ansible.core.hypervizor.com>
2019-07-28 07:43:08 -0700

    added config check

--- attach_t1.py
+++ attach_t1.py
@@ -20,23 +20,35 @@ nsxtmgr = "nsxtmgr01"
 auth = HTTPBasicAuth(username, password)
 
 
-def get_t0_config(t0_gw):
-  url = 'https://nsxtmgr01/policy/api/v1/infra/tier-0s/' + t0_gw  
+def get_t1_config():
+  url = 'https://' + nsxtmgr + '/policy/api/v1/infra/tier-1s/' + t1_gw  
   response = requests.get(url, verify=False, auth=auth)
   current_config = json.loads(response.text)
   return current_config
 
-def attach_t0(t0_path):
+def check_config_state(current_config):
+    if 'tier0_path' in current_config:
+        if current_config['tier0_path'] == '/infra/tier-0s/' + t0_gw:
+            return False
+        else:
+            return True
+    else:
+        return True
+
+def attach_t0():
     url = 'https://' + nsxtmgr + '/policy/api/v1/infra/tier-1s/' + t1_gw    
-    payload = { "tier0_path": "" + t0_path + "" }
+    payload = { "tier0_path": '/infra/tier-0s/' + t0_gw }
     response = requests.patch(url, data=json.dumps(payload), headers=headers,  verify=False, auth=auth)
     return response
 
 def main(): 
-    t0_config = get_t0_config(t0_gw)
-    t0_path = t0_config['path']
-    response = attach_t0(t0_path)
-    print response.status_code
+    current_config = get_t1_config()
+    t1_state = check_config_state(current_config)
+    if t1_state  == True:
+        response = attach_t0()
+        print response.status_code
+    else:
+        pass
 
 
 if __name__ == '__main__':
