ff797297ff897d5193c0a4d13da3a6e8918b79f1
Matthew Pinizzotto <opusdefunk@hotmail.com>
2021-04-08 19:59:02 -0400

    Fix "tags" key error

--- set_vm_tag.py
+++ set_vm_tag.py
@@ -1,34 +1,37 @@
 #usr/bin/env python
 #coding utf-8
 
-_author_ = "mpinizzotto"
-
-"""
-
-pulls from csv file (name, scope, tag), updates tags on virtual machine.
-
-"""
-
 import csv
 import requests
 import json
 from requests.auth import HTTPBasicAuth
 requests.packages.urllib3.disable_warnings()
 
-headers = { 'content-type': 'application/json' }
+_author_ = "mpinizzotto"
+
+"""
+
+pulls from csv file (name, scope, tag), adds tags to virtual machine.
+
+"""
+
+headers = {'content-type': 'application/json'}
 username = 'admin'
-password = 'VMware1!VMware1!'
-nsxtmgr = "nsxtmgr01"
+password = 'mypassword'
+nsxtmgr = "nsxmgr"
 auth = HTTPBasicAuth(username, password)
 
 
 def get_config(line):
     vm_name = line.get('name')
-    url = "https://" + nsxtmgr + "/api/v1/fabric/virtual-machines/?display_name=" + vm_name 
+    url = "https://" + nsxtmgr + "/api/v1/fabric/virtual-machines/?display_name=" + vm_name
     response = requests.get(url, verify=False, auth=auth)
     parse = json.loads(response.text)
     external_id = parse['results'][0]['external_id']
-    current_tags = parse['results'][0]['tags']
+    if 'tags' in parse['results'][0]:
+        current_tags = parse['results'][0]['tags']
+    else:
+        current_tags = None
     return external_id, current_tags
 
 
@@ -40,10 +43,9 @@ def update_tags(external_id, line, current_tags):
         for tags in current_tags:
             new_tags.append(tags)
     else:
-        pass 
-    
+        pass
     url = "https://" + nsxtmgr + "/api/v1/fabric/virtual-machines?action=update_tags"
-    payload =  {
+    payload = {
         "external_id": external_id,
         "tags": new_tags
     }
@@ -64,23 +66,24 @@ def read_from_csv(file):
 def normalize_tag_list(item_list):
     tag_list = []
     for line in item_list:
-        vm_info = {}
-        vm_info['name']= line[0]
-        vm_info['scope'] = line[1]
-        vm_info['tag'] = line[2]  
-        tag_list.append(vm_info)
+        if line == []:
+            continue
+        else:
+            vm_info = {}
+            vm_info['name'] = line[0]
+            vm_info['scope'] = line[1]
+            vm_info['tag'] = line[2]
+            tag_list.append(vm_info)
     return tag_list
 
 
 def main():
-    item_list = read_from_csv('vm_tags.csv')
+    item_list = read_from_csv('tags.csv')
     tag_list = normalize_tag_list(item_list)
-
     for line in tag_list:
         external_id, current_tags = get_config(line)
         response, new_tags = update_tags(external_id, line, current_tags)
         print response, new_tags
-
 
 if __name__ == '__main__':
     main()
